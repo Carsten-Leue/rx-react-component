70deb7462732f1543d6eaa44f2a6df238bf64451
Dr. Carsten Leue <carsten.leue@de.ibm.com>
2019-10-04 16:45:05 +0200

    add testing

--- src/app.spec.tsx
+++ src/app.spec.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import { render } from '@testing-library/react';
+import React, { PureComponent } from 'react';
+import { distinctUntilChanged, map, pluck } from 'rxjs/operators';
+
 import { RxComponent } from './rx.component';
-import { pluck, distinctUntilChanged, map } from 'rxjs/operators';
+import { rxComponent } from './rx.hoc';
 
 interface SampleProps {
   sampleInput: string;
@@ -28,12 +30,31 @@ class SampleApp extends RxComponent<SampleProps, SampleState> {
 
   render() {
     const { state } = this;
-    return <div>{state.sampleState}</div>;
+    return <div data-testid="sample-id">{state.sampleState}</div>;
   }
 }
 
+const ViewApp = (aProps: SampleState) => (
+  <div data-testid="sample-id">{aProps.sampleState}</div>
+);
+
 describe('app', () => {
+  it('renders HOC', () => {
+    const MyApp = rxComponent<SampleProps, SampleState>(
+      (props$) =>
+        props$.pipe(
+          pluck('sampleInput'),
+          distinctUntilChanged(),
+          map((prop) => ({ sampleState: `computed ${prop}` }))
+        ),
+      ViewApp
+    );
+    const { getByTestId } = render(<MyApp sampleInput={'Carsten'} />);
+    expect(getByTestId('sample-id')).toHaveTextContent('computed Carsten');
+  });
+
   it('renders without crashing', () => {
-    shallow(<SampleApp sampleInput={'Carsten'} />);
+    const { getByTestId } = render(<SampleApp sampleInput={'Carsten'} />);
+    expect(getByTestId('sample-id')).toHaveTextContent('computed Carsten');
   });
 });
