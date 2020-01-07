67bc0e08a71e6416a74e3d80ac1db4196e7e0bc8
Dr. Carsten Leue <carsten.leue@gmx.net>
2020-01-07 15:35:55 +0100

    improve on size

--- src/di/provider.ts
+++ src/di/provider.ts
@@ -1,4 +1,4 @@
-import { Context, createElement, FC, ReactElement } from 'react';
+import { Context, createElement, FC, ReactNode } from 'react';
 import { UnaryFunction } from 'rxjs';
 
 import { ReactModule, ReactModuleProps } from './module';
@@ -96,10 +96,8 @@ const topoSort = <K, V>(
  *
  * @returns the new element
  */
-const reduceModule = (
-  aChildren: ReactElement,
-  { module }: ReactProvider<any>
-) => createElement(module, null, aChildren);
+const reduceModule = (aChildren: ReactNode, { module }: ReactProvider<any>) =>
+  createElement(module, null, aChildren);
 
 /**
  * Constructs a module from a topological list of providers
@@ -111,10 +109,10 @@ function createProviderModule(
   aProviders: Array<ReactProvider<any>>
 ): FC<ReactModuleProps> {
   // extract the start module and the providers
-  const [{ module }, ...providers] = aProviders;
+  const [root, ...providers] = aProviders;
   // construct the component tree
   return ({ children }) =>
-    providers.reduce(reduceModule, createElement(module, null, children));
+    providers.reduce(reduceModule, reduceModule(children, root));
 }
 
 /**
